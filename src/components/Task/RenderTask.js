8f154bbe023f902d0f7cbf2f155f76ff93f3db28
Kévin Joya <kevin.joya@outlook.com>
2019-12-24 19:36:23 +0100

    Added some logique to render with status

--- src/components/Task/RenderTask.js
+++ src/components/Task/RenderTask.js
@@ -1,58 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import store from "../../redux/store";
 
 function RenderTask() {
-  // const [data, setData] = useState(store.getState());
-  const [data, setData] = useState({
-    tasks: store.getState()
-  });
+  const [data, setData] = useState(store.getState());
   store.subscribe(() => setData(store.getState()));
 
+  if (data.tasks.length === 0) return null;
 
-  function completedTask(e, index) {
-    e.target.parentNode.classList.add("task--completed");
-    console.log(e.target);
-    console.log(e.index);
-    
-    console.log(e.target.id);
-
-    const newState = [...data];
-    console.log(newState);
-
-    newState.map((task, index) => {
-      task.status = "completed";
-    })
-    setData(newState)
-    
-
-    // data.map((task, index) => {
-    //   console.log(data.indexOf(task));
-    //   console.log(task);
-    // })
-  }
-
-  console.log(data);
-  return(
-    <React.Fragment>
-      {
-        function () {
-          if(data.tasks.length === 0) return null;
-
-          if (data.tasks.length !== 0) {
-            return data.tasks.map((task, index) => {
-              return(
-                <div className="task" key={index}>
-                  <input type="checkbox" className="mr-3 mt-2" onClick={(e, index) => completedTask(e, index) } />
-                  <p>{task.task}</p>
-                </div>
-                )
-              })
-          }
-
-        }()
+  if (data.tasks.length !== 0) {
+    return data.tasks.map((task, index) => {
+      if (task.status === data.visibility ) {
+        return (
+          <div className={`task ${task.status === "Completed" ? "task--completed" : ""}`} key={index}>
+            <input type="checkbox" className="mr-3 mt-2" onClick={() => store.dispatch({ type: "TASK_COMPLETED", taskIndex: index })} />
+            <p>{task.task}</p>
+          </div>
+        )
       }
-    </React.Fragment>
-  )
+    })
+  }
 }
 
 export default RenderTask
